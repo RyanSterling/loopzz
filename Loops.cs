ba134b628a4c2969399145441c120bd5cae537ac
RyanSterling <rsterling20@gmail.com>
2016-11-12 14:39:57 -0700

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Loop_Examples1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num, i, result;
+			Console.Write("Enter a number\t");
+			num = Convert.ToInt32(Console.ReadLine());
+
+			for (i = 1; i <= 10; i++)
+			{
+				result = num * i;
+				Console.WriteLine("{0} x {1} = {2}", num, i, result);
+			}
+			Console.ReadLine();
+		}
+	}
+}
+	
+
+
